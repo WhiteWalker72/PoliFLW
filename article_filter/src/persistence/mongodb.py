4fd20ad62835e1d981c0f15f0093db485cdb04d7
WhiteWalker72 <sivarwerrie@hotmail.nl>
2019-03-14 10:31:33 +0100

    Pushing to different database

--- article_filter/src/persistence/mongodb.py
+++ article_filter/src/persistence/mongodb.py
@@ -2,7 +2,7 @@ import pymongo
 import urllib
 
 client = pymongo.MongoClient("mongodb+srv://admin:" + urllib.parse.quote("poliflw9@") + "@poliflw-p58zy.mongodb.net/test?retryWrites=true")
-db = client["poliflw"]
+db = client["article_filter_service"]
 articles_collection = db["articles"]
 
 articles_collection.create_index([('text', 'text'), ('title', 'text')])
